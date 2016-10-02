f5d8d9365e06a89c705e10e2958c3691e8b43cf1
e-hartung <elizabeth.hartung@gmail.com>
2016-10-02 14:07:04 -0400

    Create util/main.php for app path

---
+++ util/main.php
@@ -0,0 +1,11 @@
+<?php
+// Get the document root
+$doc_root = filter_input(INPUT_SERVER, 'DOCUMENT_ROOT', FILTER_SANITIZE_STRING);
+
+// Get the application path
+$uri = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_STRING);
+$dirs = explode('/', $uri);
+$app_path = '/' . $dirs[1] . '/' . $dirs[2] . '/' . $dirs[3] . '/';
+
+// Set the include path
+set_include_path($doc_root . $app_path);
