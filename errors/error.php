ea95a0fcdcd0de799a8cbbc9b75956e9d9ba853f
e-hartung <elizabeth.hartung@gmail.com>
2016-10-29 21:53:16 -0400

    Add function to display non-database error messages

--- errors/error.php
+++ errors/error.php
@@ -1,6 +1,6 @@
 <?php include '../view/header.php'; ?>
 <div id="main">
     <h1 class="top">Error</h1>
-    <p><?php echo $error; ?></p>
+    <p><?php echo $error_message; ?></p>
 </div>
 <?php include '../view/footer.php'; ?>
--- util/main.php
+++ util/main.php
@@ -10,5 +10,12 @@ $app_path = '/' . $dirs[1] . '/' . $dirs[2] . '/' . $dirs[3] . '/';
 // Set the include path
 set_include_path($doc_root . $app_path);
 
+// Function for non-database errors
+function display_error($error_message) {
+    global $app_path;
+    include 'errors/error.php';
+    exit;
+}
+
 // Start session
 session_start();
