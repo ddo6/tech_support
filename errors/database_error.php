4ad62d6b550829673943ba7fd10bc4485d666655
e-hartung <elizabeth.hartung@gmail.com>
2016-10-29 20:13:57 -0400

    Edit text on database error page

--- errors/database_error.php
+++ errors/database_error.php
@@ -1,10 +1,8 @@
 <?php include '../view/header.php'; ?>
 <div id="main">
     <h1 class="top">Database Error</h1>
-    <p>There was an error connecting to the database.</p>
-    <p>The database must be installed as described in appendix A.</p>
-    <p>The database must be running as described in chapter 1.</p>
-    <p>Error message: <?php echo $error_message; ?></p>
+    <p>An error occurred while attempting to work with the database.</p>
+    <p>Message: <?php echo $error_message; ?></p>
     <p>&nbsp;</p>
 </div><!-- end main -->
 <?php include '../view/footer.php'; ?>
