2891c3932d1da8c3ecd1c605804542b46598f6c5
e-hartung <elizabeth.hartung@gmail.com>
2016-10-29 20:13:24 -0400

    Add database error function

--- model/database_oo.php
+++ model/database_oo.php
@@ -3,6 +3,7 @@ class Database {
     private static $dsn = 'mysql:host=localhost;dbname=tech_support';
     private static $username = 'root';
     private static $password = '';
+    private static $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
     private static $db;
 
     private function __construct() {}
@@ -12,7 +13,8 @@ class Database {
             try {
                 self::$db = new PDO(self::$dsn,
                                     self::$username,
-                                    self::$password);
+                                    self::$password,
+                                    self::$options);
             } catch (PDOException $e) {
                 $error_message = $e->getMessage();
                 include('../errors/database_error.php');
@@ -20,5 +22,11 @@ class Database {
             }
         }
         return self::$db;
+    }
+    
+    public static function display_db_error($error_message) {
+        global $app_path;
+        include('../errors/database_error.php');
+        exit();
     }
 }
