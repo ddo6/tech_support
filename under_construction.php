4241deedc9d6592b579fcf4f30009d110a5e1fad
e-hartung <elizabeth.hartung@gmail.com>
2016-10-30 22:44:08 -0400

    Remove Under Construction file

--- under_construction.php
+++
@@ -1,6 +0,0 @@
-<?php include 'view/header_root.php'; ?>
-<div id="main">
-    <h2>Sorry, this page is currently under construction.</h2>
-    <p>We'll finish it as quickly as we can. Thanks!</p>
-</div>
-<?php include 'view/footer.php'; ?>
